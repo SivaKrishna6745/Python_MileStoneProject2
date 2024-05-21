30c779ca79e46a8c33d2220510db014322ff6b25
sivakrishna <sivakrishna.m@teamx.one>
2024-05-21 15:05:42 +0530

    MilestoneProject 2 - books database using csv files

---
+++ app_csv.py
@@ -0,0 +1,57 @@
+from utils import database_csv
+
+USER_CHOICE = """
+Enter:
+- 'a' to add a new book
+- 'l' to list all books
+- 'r' to mark a book as read
+- 'd' to delete a book
+- 'q' to quit
+Your Choice: """
+
+
+# def prompt_add_book() - ask for book name and author and add it to the list of books
+def prompt_add_book():
+    book = input("Enter book name, author separated by commas: ")
+    name = book.split(',')[0]
+    author = book.split(',')[1]
+    database_csv.add_book({
+        'name': name,
+        'author': author,
+        'read': False
+    })
+
+
+# def list_all_books() - show all the books in the list
+def list_all_books():
+    print(database_csv.list_books())
+
+
+# def mark_book_read() - ask for book name and mark it as read in the list
+def mark_book_read():
+    book = input("Enter book name that you wanted to mark as read: ")
+    return database_csv.mark_book(book)
+
+
+# def delete_a_book() - ask for book name and delete the book from the list
+def delete_a_book():
+    book = input("Enter book name that you wanted to delete from list: ")
+    return database_csv.delete_book(book)
+
+
+operations = {
+    'a': prompt_add_book,
+    'l': list_all_books,
+    'r': mark_book_read,
+    'd': delete_a_book
+}
+
+
+def menu():
+    user_input = input(USER_CHOICE)
+    while user_input != 'q':
+        operations[user_input]()
+        user_input = input(USER_CHOICE)
+
+
+menu()
---
+++ utils/database_csv.py
@@ -0,0 +1,49 @@
+"""
+Concerned with storing and retrieving books from a csv file
+
+Format of the csv file:
+name,author,read
+"""
+
+books_file = 'books.txt'
+
+
+def add_book(book):
+    with open(books_file, 'a') as file:  # 'a' mode - append mode for a file to add at the end
+        file.write(f"{book['name']}, {book['author']}, 'False'\n")
+
+
+def list_books():
+    with open(books_file, 'r') as file:
+        file_content = [book.strip().split(',') for book in file.readlines()]
+    return [
+        {'name': book[0], 'author': book[1], 'read': book[2]}
+        for book in file_content
+    ]
+
+
+def mark_book(name):
+    books = list_books()
+    for book in books:
+        if book['name'] == name:
+            book['read'] = True
+    _save_all_books(books)  # _ at the start represents that this function should be called in this file only (kind of private function)
+    # else:
+    #     print(f'book with name, {name}, not found')
+
+
+def _save_all_books(books):
+    with open(books_file, 'w') as file:
+        for book in books:
+            file.write(f"{book['name']},{book['author']},{book['read']}\n")
+
+
+def delete_book(name):
+    books = list_books()
+    books_updated = []
+    for book in books:
+        if book['name'] != name:
+            books_updated.append(book)
+    _save_all_books(books_updated)
+    # else:
+    #     print(f'book with name, {name}, not found')
